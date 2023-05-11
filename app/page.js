a053cb673a6aed5165712b1e5a0c4239c82f4bae
Estéfane Souza <53922973+estefanecs@users.noreply.github.com>
2023-05-11 11:23:28 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,95 +1,81 @@
+"use client"; 
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import React from 'react';
+import { useState } from 'react';
+
+
+//Coloque o código dos demais componentes aqui...
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    
+  const [blogMessages, setBlogMessages] = useState([]);
+  
+  fetch('https://script.google.com/macros/s/AKfycbzBn3sALe1rYjz7Ze-Ik7q9TEVP0I2V3XX7GNcecWP8NvCzGt4yO_RT1OlQp09TE9cU/exec')
+    .then(response => response.json())
+    .then(data => {
+        setBlogMessages(data);
+    });
+    
+    return (
+      <main className={styles.main}>
+         <SearchBar filterText={filterText}  inStockOnly={inStockOnly} 
+      onFilterTextChange={setFilterText}
+      onInStockOnlyChange={setInStockOnly}/>
+        <FilterableMessageTable messages={blogMessages} />
+      </main>
+    )
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      function FilterableMessageTable({ messages}) {
+        const rows = [];
+        let lastCategory = null;
+        messages.forEach((message) => {
+          rows.push(
+            <MessageRow
+              message={message}
+              key={message.author} />
+          );
+          lastCategory = product.category;
+        });
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        return (
+          <table>
+            <thead>
+              <tr>
+                <th>Author</th>
+                <th>Message</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
+        );
+      }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    function MessageRow({ message }) {
+      return (
+        <tr>
+          <td>{message.author}</td>
+          <td>{message.message}</td>
+          <td>{message.date}</td>
+        </tr>
+      );
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+    ///PESQUISA
+    function SearchBar({filterText, inStockOnly, onFilterTextChange,
+      onInStockOnlyChange}) {
+      return (
+        <form>
+          <label>
+            Procure uma mensagem:
+          </label>
+          <input type="text" value={filterText} placeholder="Search..." 
+          onChange={(e) => onFilterTextChange(e.target.value)}/>
+        </form>
+      );
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
 }
+
